63697f9b39dcfa8f7728274ea23bb328e76b508d
Danila Shutov <dcbrwn2@gmail.com>
2019-02-12 01:11:02 +0300

    Use external framework for DOM rendering

--- index.js
+++ index.js
@@ -1,43 +1,152 @@
-const canvas = document.getElementById('canvas');
-const content = document.getElementById('content');
-const scrollContainer = document.getElementById('scroll-container')
-const glsl = new GlslCanvas(canvas);
-let sketchCounter = 0;
+import { render, html } from 'https://unpkg.com/lighterhtml@0.9.0?module';
 
-function loadNextSketch () {
-  const sketch = sketches[sketchCounter++ % sketches.length];
-  loadSketch(sketch);
-}
+!function main () {
+  let canvas;
+  let glsl;
+  let selectedSketch = sketches[sketches.length - 1];
+  let context = {
+    title: 'Sketchpad',
+    content: { html: '' },
+  };
 
-async function loadSketch (sketch) {
-  try {
-    document.title = sketch.title;
+  async function loadSketch (sketch) {
+    try {
+      document.title = sketch.title;
 
-    const response = await fetch(sketch.frag);
-    const frag = await response.text();
-    glsl.load(frag);
-    glsl.play();
-    requestAnimationFrame(() => glsl.pause());
+      if (!sketch._frag) {
+        const response = await fetch(sketch.frag);
+        const frag = await response.text();
+        sketch._frag = frag;
+      }
 
-    content.innerHTML = sketch.note;
-  } catch (error) {
-    // So useful. Much sense. Wow
-    console.warn('Failed to load sketch', error);
+      glsl.load(sketch._frag);
+      glsl.play();
+      requestAnimationFrame(() => glsl.pause());
+
+      context.title = sketch.title;
+      context.createdAt = sketch.createdAt;
+      context.content.html = sketch.note;
+      updateDom();
+    } catch (error) {
+      // So useful. Much sense. Wow
+      console.warn('Failed to load sketch', error);
+    }
   }
-}
 
-function main () {
-  glsl.on('error', (event) => {
-    console.warn('GLSL-canvas error:', event);
-  });
-  
-  scrollContainer.addEventListener('click', (event) => {
-    if (event.target === scrollContainer) {
+  function handleScrollContainerClick (event) {
+    if (event.target === event.currentTarget) {
       glsl.toggle();
     }
-  });
+  }
 
-  loadSketch(sketches[sketches.length - 1]);
-}
+  function handleSketchChange (event) {
+    selectedSketch = sketches.find((sketch) => sketch.createdAt === event.target.value);
+    loadSketch(selectedSketch);
+  }
 
-main();
+  function updateDom () {
+    render(document.body, () => html`
+      <style>
+        html, body {
+          padding: 0;
+          margin: 0;
+          font-family: 'Times New Roman', Times, serif;
+          font-size: 14px;
+          overflow: hidden;
+        }
+
+        #canvas {
+          position: absolute;
+          z-index: 1;
+          top: 0;
+          left: 0;
+          width: 100vw;
+          height: 100vh;
+          image-rendering: -moz-crisp-edges;
+          image-rendering: pixelated;
+          -ms-interpolation-mode: nearest-neighbor;
+        }
+
+        #scroll-container {
+          cursor: pointer;
+          position: absolute;
+          z-index: 2;
+          top: 0;
+          left: 0;
+          box-sizing: border-box;
+          height: 100vh;
+          width: calc(100vw + 100px);
+          overflow: auto;
+          padding: 100vh 0 0 0;
+        }
+
+        #content-container {
+          cursor: auto;
+          width: 100vw;
+          background-color: white;
+        }
+
+        #content {
+          padding: 18px;
+          box-sizing: border-box;
+          width: 100vw;
+          max-width: 560px;
+          margin: 0 auto;
+        }
+
+        #sketch-selector {
+          margin-left: -4px;
+          border: 0;
+          padding: 0;
+          font-family: 'Times New Roman', Times, serif;
+          font-size: 18px;
+          font-weight: bold;
+          background: transparent;
+          outline: none;
+        }
+
+        #sketch-selector option {
+          font-size: 14px;
+        }
+
+        #date {
+          clear: both;
+          float: right;
+        }
+      </style>
+
+      <canvas id="canvas"></canvas>
+
+      <div id="scroll-container" onclick=${handleScrollContainerClick}>
+        <div id="content-container">
+          <section id="content">
+            <article>
+              <select id="sketch-selector" onchange=${handleSketchChange}>
+                ${sketches.map((sketch) => html`
+                  <option
+                    selected=${selectedSketch.createdAt === sketch.createdAt}
+                    value=${sketch.createdAt}>${sketch.title}</option>
+                `)}
+              </select>
+              ${context.content}
+            </article>
+          </section>
+        </div>
+      </div>
+    `);
+  }
+
+  function init () {
+    updateDom();
+
+    canvas = document.getElementById('canvas');
+    glsl = new GlslCanvas(canvas);
+    glsl.on('error', (event) => {
+      console.warn('GLSL-canvas error:', event);
+    });
+
+    loadSketch(selectedSketch);
+  }
+
+  init();
+}();
--- sketches.js
+++ sketches.js
@@ -4,19 +4,16 @@ window.sketches = [
     frag: '/sketches/0001.glsl',
     title: 'Ray-marching scene',
     note: `
-      <article>
-        <h1>A basic ray-marching scene</h3>
-        <p>
-          Ray-marching is an interesting rendering technique, I found out a few years ago in articles of
-          <a target="_blank" href="http://iquilezles.org/www/articles/raymarchingdf/raymarchingdf.htm">Inigo Quilez</a>.
-          With modern hardware, it allows amateurs like me, to experience a joy of creating phororealistic real-time procedural art,
-          only requiring some basic linear algebra knowledge.
-        </p>
-        <p>
-          Lighting artifacts in this scene are consequences of using naive shading. I need to figure out why this happends and fix it in
-          future sketches.
-        </p>
-      </article>
+      <p>
+        Ray-marching is an interesting rendering technique, I found out a few years ago in articles of
+        <a target="_blank" href="http://iquilezles.org/www/articles/raymarchingdf/raymarchingdf.htm">Inigo Quilez</a>.
+        With modern hardware, it allows amateurs like me, to experience a joy of creating phororealistic real-time procedural art,
+        only requiring some basic linear algebra knowledge.
+      </p>
+      <p>
+        Lighting artifacts in this scene are consequences of using naive shading. I need to figure out why this happends and fix it in
+        future sketches.
+      </p>
     `,
   },
   {
@@ -24,12 +21,9 @@ window.sketches = [
     frag: '/sketches/0002.glsl',
     title: 'Cellular noise',
     note: `
-      <article>
-        <h1>Cellular noise</h3>
-        <p>
-          A nice, organic noise function. Fun to play with. Just by picking different colors one can get interesting results.
-        </p>
-      </article>
+      <p>
+        A nice, organic noise function. Fun to play with. Just by picking different colors one can get interesting results.
+      </p>
     `,
   },
   {
@@ -37,12 +31,9 @@ window.sketches = [
     frag: 'sketches/0003.glsl',
     title: 'Trippy wallpaper',
     note: `
-      <article>
-        <h1>Trippy wallpaper</h3>
-        <p>
-          <code>(1.0 + cos(p.x * p.y + u_time * 10.0 + p.x * p.x)) / 2.0;</code>
-        </p>
-      </article>
+      <p>
+        <code>(1.0 + cos(p.x * p.y + u_time * 10.0 + p.x * p.x)) / 2.0;</code>
+      </p>
     `,
   }
 ];
